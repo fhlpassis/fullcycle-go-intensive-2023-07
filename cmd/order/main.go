70bcae20f1b6ac92e95dcb609383c3c52ead7424
Paulo Assis <fhlpassis@gmail.com>
2023-08-12 21:34:57 -0300

    chore: add rabbitmq

--- cmd/order/main.go
+++ cmd/order/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
 
 	"github.com/fhlpassis/fullcycle-go-intensivo-2023-07/internal/infra/database"
 	"github.com/fhlpassis/fullcycle-go-intensivo-2023-07/internal/usecase"
-
+	"github.com/fhlpassis/fullcycle-go-intensivo-2023-07/pkg/rabbitmq"
 	_ "github.com/mattn/go-sqlite3"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 type Car struct {
@@ -23,6 +26,14 @@ func main() {
 
 	orderRepository := database.NewOrderRepository(db)
 	uc := usecase.NewCalculateFinalPrice(orderRepository)
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+	msgRabbitmqChannel := make(chan amqp.Delivery)
+	go rabbitmq.Consume(ch, msgRabbitmqChannel)
+	rabbitmqWorker(msgRabbitmqChannel, uc)
 
 	input := usecase.OrderInput{
 		ID:    "123",
@@ -35,4 +46,21 @@ func main() {
 		panic(err)
 	}
 	println(output)
+}
+
+func rabbitmqWorker(msgChan chan amqp.Delivery, uc *usecase.CalculateFinalPrice) {
+	fmt.Println("Starting rabbitmq")
+	for msg := range msgChan {
+		var input usecase.OrderInput
+		err := json.Unmarshal(msg.Body, &input)
+		if err != nil {
+			panic(err)
+		}
+		output, err := uc.Execute(input)
+		if err != nil {
+			panic(err)
+		}
+		msg.Ack(false)
+		fmt.Println("Message processed and saved to database:", output)
+	}
 }
--- internal/usecase/calculate_price.go
+++ internal/usecase/calculate_price.go
@@ -3,10 +3,12 @@ package usecase
 import "github.com/fhlpassis/fullcycle-go-intensivo-2023-07/internal/entity"
 
 type OrderInput struct {
-	ID    string
-	Price float64
-	Tax   float64
+	ID    string  `json:"id"`
+	Price float64 `json:"price"`
+	Tax   float64 `json:"tax"`
 }
+
+// {"id": "1", "price": 10.0, "tax": 0.1}
 
 type OrderOutput struct {
 	ID         string
